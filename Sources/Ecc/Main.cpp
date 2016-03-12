9f0a3e9af81ccfe09fa0348424dd8973747f36b1
Robert MacGregor <ragoradev@gmail.com>
2016-03-12 17:36:38 -0500

    Check for NULL return from malloc in es vertification code and print error if es verification fails

--- Sources/Ecc/Main.cpp
+++ Sources/Ecc/Main.cpp
@@ -252,6 +252,10 @@ ESStatus GetESStatus()
         return result;
 
     char* temporaryBuffer = (char*)malloc(length);
+
+    if (!temporaryBuffer)
+        return ESStatus::Error;
+
     fseek(_fInput, 0, SEEK_SET);
     fread(temporaryBuffer, length, 1, _fInput);
     fseek(_fInput, 0, SEEK_SET);
@@ -398,10 +402,11 @@ int main(int argc, char *argv[])
       case ESStatus::Error:
       {
           fclose(_fInput);
+          printf("Ecc encountered an error during the es verification.\n");
           return EXIT_FAILURE;
       }
   }
-
+  
   //printf("%s\n", argv[1]);
   // open all the output files
   char *strImplementation    = ChangeFileNameExtension(argv[1], ".cpp_tmp");
